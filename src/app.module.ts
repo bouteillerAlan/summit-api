4039bf950260c89fa57ad3d4f9dda6b2abeba86e
a2n <a2n.dev@pm.me>
2023-02-25 00:33:53 +0100

    add connector to mysql db

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Training } from './training/training.entity';
+import { TrainingModule } from './training/training.module';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: '127.0.0.1',
+      port: 3306,
+      username: 'root',
+      password: '',
+      database: 'summit',
+      entities: [Training],
+      synchronize: true,
+    }),
+    TrainingModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
