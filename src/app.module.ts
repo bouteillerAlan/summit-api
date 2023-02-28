168d8b0ab0924d2ffe3455f79c3b2eb54e45bae9
a2n <a2n.dev@pm.me>
2023-02-28 18:24:48 +0100

    autoLoad entities
    https://docs.nestjs.com/techniques/database#auto-load-entities

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,8 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { Training } from './training/training.entity';
 import { TrainingModule } from './training/training.module';
 import { ExerciseModule } from './exercise/exercise.module';
-import { Exercise } from './exercise/exercise.entity';
-import { ExerciseType } from './exerciseType/exerciseType.entity';
 import { ExerciseTypeModule } from './exerciseType/exerciseType.module';
 
 @Module({
@@ -18,7 +15,7 @@ import { ExerciseTypeModule } from './exerciseType/exerciseType.module';
       username: 'root',
       password: '',
       database: 'summit',
-      entities: [Training, Exercise, ExerciseType],
+      autoLoadEntities: true,
       synchronize: true
     }),
     TrainingModule,
