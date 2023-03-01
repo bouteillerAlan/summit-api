046a414418005b0da41e52ce10941d2d851bbd3d
a2n <a2n.dev@pm.me>
2023-03-01 18:20:25 +0100

    wip build uniqueness validator

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
+import { useContainer } from 'class-validator';
 
 /**
  * bootstrap the app
@@ -8,19 +10,35 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule);
 
+  // whitelist: removes all properties of a request’s body which are not in the DTO
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+
+  // for custom decorator in class validator
+  useContainer(app.select(AppModule), { fallbackOnErrors: true });
+
   const config = new DocumentBuilder()
     .setTitle('Summit')
     .setDescription('Summit api')
     .setVersion('1.0')
     .build();
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+  SwaggerModule.setup('api', app, document, {
+    swaggerOptions: {
+      docExpansion: 'none',
+      displayRequestDuration: true,
+      filter: true
+    }
+  });
 
   await app.listen(3000);
 }
 
-bootstrap().then((): void => {
-  console.log('started');
-}).catch(e => {
-  console.log('error', e);
-});
+/* eslint-disable no-console */
+bootstrap()
+  .then((): void => {
+    console.log('started');
+  })
+  .catch(e => {
+    console.log('error', e);
+  });
+/* eslint-enable no-console */
---
+++ src/validator/isUnique.validator.ts
@@ -0,0 +1,57 @@
+import {
+  ValidatorConstraint,
+  type ValidatorConstraintInterface,
+  type ValidationArguments,
+  type ValidationOptions, registerDecorator
+} from 'class-validator';
+import { type ExerciseType } from '../exerciseType/exerciseType.entity';
+import { Repository } from 'typeorm';
+import { Injectable } from '@nestjs/common';
+
+/**
+ * register the decorator
+ * @param {ValidationOptions} validationOptions the options you want to pass to the validator
+ * @returns {void}
+ */
+export function IsUniqueExerciseTypeName(validationOptions?: ValidationOptions) {
+  return (object: object, propertyName: string): void => {
+    registerDecorator({
+      target: object.constructor,
+      propertyName,
+      options: validationOptions,
+      validator: IsUniqueClass
+    });
+  };
+}
+
+// named IsUnique*Class* because I want to use @IsUnique for my decorator name setup above
+@ValidatorConstraint({ name: 'isUnique', async: true })
+@Injectable()
+export class IsUniqueClass implements ValidatorConstraintInterface {
+  constructor(
+    private readonly exerciseTypeRepository: Repository<ExerciseType>
+  ) {}
+
+  /**
+   * check that the given value is unique for the given field and table
+   * @param {unknown} value the tested value
+   * @param {ValidationArguments} args the arguments of the demand injected via the decorator
+   * @returns {boolean} if the value is unique or not
+   */
+  async validate(value: unknown, args: ValidationArguments): Promise<boolean> {
+    // todo make it generic
+    // console.log('validator args >>>>>>>>>> ', { value, args });
+    const r = await this.exerciseTypeRepository.find({ where: { name: value as string } });
+    return false;
+  }
+
+  /**
+   * error message if the validation failed
+   * @param {ValidationArguments} args the arguments of the demand injected via the decorator
+   * @returns {string} the error message
+   */
+  defaultMessage(args: ValidationArguments): string {
+    const { value } = args;
+    return `The value '${value as string}' already exist`;
+  }
+}
